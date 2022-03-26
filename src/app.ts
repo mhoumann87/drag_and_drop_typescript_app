9a4b67276244b286fb4d8c2b13a996a56e74fbdc
Michael Houmann <mhoumann87@gmail.com>
2022-03-26 09:23:41 +0100

    Add the beginning of ProjectList class

--- src/app.ts
+++ src/app.ts
@@ -80,6 +80,45 @@ const validate = (validatableInput: Validatable) => {
 };
 
 // ? Classes
+
+// ? Rendering the List
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById(
+      'project-list'
+    )! as HTMLTemplateElement;
+
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector(
+      'h2'
+    )!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
+
 //? Render the form on the web page
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -198,5 +237,6 @@ class ProjectInput {
 
 // ? Instantiate the ProjectInput class
 const prjInput = new ProjectInput();
-
-// ?
+// ? Instantiate the ProjectLists
+const activeProjList = new ProjectList('active');
+const finishedProjList = new ProjectList('finished');
